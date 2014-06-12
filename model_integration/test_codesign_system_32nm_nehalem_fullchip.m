81df229a2f604c7115b811b851c5dc4fc08391c6
wwahby <w.wahby@gmail.com>
2014-06-11 21:06:57 -0400

    Added missing simulation options to 32nm Nehalem test case
    use_graphene and separate_wiring_tiers were added later for additional
    control over the innards of the virtual platform

--- model_integration/test_codesign_system_32nm_nehalem_fullchip.m
+++ model_integration/test_codesign_system_32nm_nehalem_fullchip.m
@@ -75,6 +75,7 @@ wire.repeater_fraction = [ 0.5 ]; % (-) fraction of optimal repeaters to insert
 wire.Beta = [0.9];              % (-v) Fraction of total clock period that a single point-to-point interconnect can consume
 wire.Beta_short = 0.25;         % (-) Beta for shortest wiring layers (used for the top down WLARI)
 wire.Rc = 0;                    % (-v) Contact resistance between tiers (can be a vector)
+wire.use_graphene = 0;
 
 %% Power supply noise model parameters
 
@@ -146,6 +147,7 @@ simulation.topdown_WLARI = 1; % Use topdown simultaneous WLA and RI (0 = use sta
 simulation.skip_psn_loops = 1; % Skip PSN TSV homing for faster debug
 simulation.draw_thermal_map = 0; % Plot thermal profile of each chip
 simulation.print_thermal_data = 0; % Output max temp in each layer to console
+simulation.separate_wiring_tiers = 1;
 
 
 
