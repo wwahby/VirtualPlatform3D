494216c03937061e24c23d9cf8d200ebdb78ebd0
wwahby <w.wahby@gmail.com>
2014-06-13 16:57:35 -0400

    WLATDRI now uses a binary search to find optimal top layer fill, rather than a naive area shrink
    Previously top layer fill was calculated at every iteration as
    top_fill = 0.9 * top_fill_old
    
    This caused issues when using GNRs, as occasionally the bottom layer
    would be underfilled, and the naive topfill shrink algorithm would
    overshoot and both increase the number of metal layers and decrease
    bottom layer fill, which is the opposite of good.
    
    Now the algorithm checks the min and max top fill options, then checks
    the midpoint and proceeds with a binary search algorithm to find the
    option which minimizes metal layer number and maximizes bottom layer
    fill factor

--- model_integration/+xcm/wlatdri.m
+++ model_integration/+xcm/wlatdri.m
@@ -1,79 +1,95 @@
 function [wire repeater] = wlatdri(chip,gate,wire)
 
-
 bottom_layer_underfilled = 1;
-min_fill_factor = 0.8; % minimum utilization of available bottom layer area
+min_fill_factor = 0.9; % minimum utilization of available bottom layer area
 top_fill_factor = wire.routing_efficiency(1);
-wire.routing_efficiency = [wire.routing_efficiency(1) wire.routing_efficiency]; % create a separate entry for first layer
+if(length(wire.routing_efficiency) == 1) % if we only have one entry for routing efficiency we need to preserve the nominal value for all the underlying layers
+    wire.routing_efficiency = [wire.routing_efficiency(1) wire.routing_efficiency]; % create a separate entry for first layer
+end
 
 % dead man counter to get out of while loop if something goes wrong
-wla_attempts = 0;
-max_wla_attempts = 20;
-min_top_fill_factor = 0.10;
+max_wla_attempts = 16;
+min_top_fill_factor = 0.01;
 
-% Keep track of the best solutions so far
-best_bot_fill_factor = 0;
-best_top_fill_factor = top_fill_factor;
-best_num_wiring_tiers = 9e12; % Absurdly large number so we immediately overwrite this;
+lbnd = min_top_fill_factor;
+rbnd = top_fill_factor;
 
-% automatically decrease top layer utilization to keep bottom layer
-% reasonably filled. Need to do this since TDWLARI doesn't guarantee
-% good usage of the bottom layer. Since this routine runs very quickly,
-% there isn't really any significant performance overhead, and this
-% lets us do a proper job of wire layer assignment and repeater
-% insertion while accurately considering wire and repeater via blockage
-while((bottom_layer_underfilled == 1) && (wla_attempts < max_wla_attempts) && (top_fill_factor > min_top_fill_factor))
+wire.wla_attempts = 2; % 1 because one of the runs below will get thrown away.
+
+wire.routing_efficiency(1) = lbnd;
+[wire_lbnd repeater_lbnd] = xcm.wlatdri_cu_gnr(chip,gate,wire);
+
+wire.routing_efficiency(1) = rbnd;
+[wire_rbnd repeater_rbnd] = xcm.wlatdri_cu_gnr(chip,gate,wire);
+
+
+bin_gen = 1;
+while( (bottom_layer_underfilled) && (bin_gen <= max_wla_attempts) )
+   
+    mid = (lbnd + rbnd)/2;
+    wire.routing_efficiency(1) = mid;
     
-    % Actually run topdown WLA and RI
-    %[wire_temp repeater_temp] = xcm.wla_topdown_with_repeaters(chip,gate,wire);
-    [wire_temp repeater_temp] = xcm.wlatdri_cu_gnr(chip,gate,wire);
-
-    % Figure out what the bottom-layer utilization is
-    bot_fill_factor = wire_temp.wire_area(1)/(wire_temp.area_per_layer(1));
-
-    % Figure out whether we need to decrease top layer use, or stop
-    if(bot_fill_factor < min_fill_factor)
-        top_fill_factor = 0.90*top_fill_factor;
-        wire.routing_efficiency(1) = top_fill_factor;
-    else
-        bottom_layer_underfilled = 0;
-    end
-
-    % If we have a better fill factor on the bottom and a smaller
-    % number of wiring tiers, this is the best solution
+    [wire_mid repeater_mid] = xcm.wlatdri_cu_gnr(chip,gate,wire);
     
-    better_wiring_tiers = length(wire_temp.pn) < best_num_wiring_tiers;
-    better_bot_fill = bot_fill_factor > best_bot_fill_factor;
+    ntiers_lbnd = length(wire_lbnd.pn);
+    ntiers_mid = length(wire_mid.pn);
+    ntiers_rbnd = length(wire_rbnd.pn);
     
+    bot_ff_lbnd = wire_lbnd.wire_area(1)/wire_lbnd.area_per_layer(1);
+    bot_ff_mid = wire_mid.wire_area(1)/wire_mid.area_per_layer(1);
+    bot_ff_rbnd = wire_rbnd.wire_area(1)/wire_rbnd.area_per_layer(1);
     
-    if( (bot_fill_factor >= best_bot_fill_factor) && (length(wire_temp.pn) <= best_num_wiring_tiers) )
-        best_bot_fill_factor = bot_fill_factor;
-        best_top_fill_factor = top_fill_factor;
-        best_num_wiring_tiers = length(wire_temp.pn);
-        wire_best = wire_temp;
-        repeater_best = repeater_temp;
+    tier_vec = [ntiers_lbnd ntiers_mid ntiers_rbnd];
+    bot_ff_vec = [bot_ff_lbnd bot_ff_mid bot_ff_rbnd];
+    [min_tiers best_choice_tiers] = min(tier_vec);
+    %[min_ff best_choice_ff] = min(bot_ff_vec);
+    
+    % First, are there multiple choices with the best number of tiers?
+    min_tiers_matches = (tier_vec == min_tiers);
+    
+    bot_ff_vec(~min_tiers_matches) = -1e9; % kill off any choices with higher tier numbers
+    
+    % Now find best ff from among min_tiers possibilities
+    [max_ff best_choice] = max(bot_ff_vec);
+    
+    if(best_choice == 1) % lbnd
+        lbnd = lbnd;
+        rbnd = mid;
         
-        fprintf('WLA_BEST: Bot_FF: %.3g \t Top_FF %.3g \t Num_tiers: %.3g\n\n',best_bot_fill_factor,best_top_fill_factor,best_num_wiring_tiers')
+        wire = wire_lbnd;
+        repeater = repeater_lbnd;
+        
+        wire_rbnd = wire_mid;
+        repeater_rbnd = repeater_mid;
+    elseif(best_choice == 3) %rbnd
+        lbnd = mid;
+        rbnd = rbnd;
+        
+        wire = wire_rbnd;
+        repeater = repeater_rbnd;
+        
+        wire_lbnd = wire_mid;
+        repeater_lbnd = repeater_mid;
+    else % mid is best
+        lbnd = lbnd;
+        rbnd = mid;
+        
+        wire = wire_mid;
+        repeater = repeater_mid;
+        
+        wire_rbnd = wire_mid;
+        repeater_rbnd = repeater_mid;
     end
-
-    fprintf('WLA: M0 Underfilled: %d \t Top fill factor: %.3g \t Bottom fill factor: %.3g \t Num tiers: %d\n\n',bottom_layer_underfilled,top_fill_factor,bot_fill_factor, length(wire_temp.pn));
-    wla_attempts = wla_attempts + 1;
+    
+    best_top_ff = wire.routing_efficiency(1);
+    bottom_layer_underfilled = (max_ff < min_fill_factor);
+    
+    if(bottom_layer_underfilled)
+        fprintf('WLA: M0 underfilled! Adjusting top layer fill factor and rerunning...')
+    end
+        
+    
+    %fprintf('WLA: M0 Underfilled: %d \t Top fill factor: %.3g \t Bottom fill factor: %.3g \t Num tiers: %d \t bin_gen %d\n\n',bottom_layer_underfilled,best_top_ff,max_ff, min_tiers, bin_gen);
+    bin_gen = bin_gen + 1;
+    
 end
-
-% If we still haven't met our target, switch to the best one we found
-if(bottom_layer_underfilled == 1)
-    fprintf('WLA Warning: Poor M0 fill factor could not be fixed. Reverting to best settings...\n\n')
-end
-% Revert settings to best found, if we aren't already there
-if(best_top_fill_factor ~= top_fill_factor)
-    fprintf('WLA: Using best settings...\n\n')
-    fprintf('WLA_BEST: Bot_FF: %.3g \t Top_FF %.3g \t Num_tiers: %.3g\n\n',best_bot_fill_factor,best_top_fill_factor,best_num_wiring_tiers')
-    wire.routing_efficiency(1) = best_top_fill_factor; % Revert to best fill factor we found previously
-    %[wire_temp repeater_temp] = xcm.wlatdri_cu_gnr(chip,gate,wire);
-    wire_temp = wire_best;
-    repeater_temp = repeater_best;
-end
-
-wire = wire_temp;
-repeater = repeater_temp;
-wire.wla_attempts = wla_attempts;
