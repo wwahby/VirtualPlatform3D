6d2d3e928a06e1265aebe4fca63cc21c1f5e9353
wwahby <w.wahby@gmail.com>
2014-06-16 12:55:36 -0400

    [CLN] Removed output spam at the end of codesign block

--- model_integration/codesign_block.m
+++ model_integration/codesign_block.m
@@ -199,13 +199,13 @@ end
 
 time_stop = cputime;
 time_elapsed = time_stop - time_start;
-disp(' ')
-disp('Final block parameters:')
-repstr = sprintf('\tNg_nom %d \t Ng_act: %d \t Atsv_nom: %.3g \t Atsv_act: %.3g \n\tN_tsvs: %d \t Npads_pow %d \t psn_nom %.4g \t psn_act %.4g', ...
-                  chip.num_gates, chip.Ng_actual, tsv.max_area_fraction, tsv.actual_area_fraction, tsv.num, psn.Npads, psn.noise_target, psn_max);
-disp(repstr)
-repstr = sprintf('\th_tsv_um: %.4g \t w_tsv_um: %.4g',h_tsv_m/1e-6,w_tsv_m/1e-6);
-disp(repstr);
+% disp(' ')
+% disp('Final block parameters:')
+% repstr = sprintf('\tNg_nom %d \t Ng_act: %d \t Atsv_nom: %.3g \t Atsv_act: %.3g \n\tN_tsvs: %d \t Npads_pow %d \t psn_nom %.4g \t psn_act %.4g', ...
+%                   chip.num_gates, chip.Ng_actual, tsv.max_area_fraction, tsv.actual_area_fraction, tsv.num, psn.Npads, psn.noise_target, psn_max);
+% disp(repstr)
+% repstr = sprintf('\th_tsv_um: %.4g \t w_tsv_um: %.4g',h_tsv_m/1e-6,w_tsv_m/1e-6);
+% disp(repstr);
 
 disp(sprintf('Total time elapsed for block design: %d',time_elapsed))
 
