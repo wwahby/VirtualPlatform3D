e9bc2406f1069ea7f1d091351a34acecce8d56b4
wwahby <w.wahby@gmail.com>
2014-06-17 19:48:42 -0400

    [PSN] Power TSVs now use size-dependent Cu resistivity

--- model_integration/+power_noise/RL_TSV.m
+++ model_integration/+power_noise/RL_TSV.m
@@ -15,7 +15,16 @@ function [R,L] = RL_TSV(H,D,rho,mu,TSVpitch,Ppitch)
 % Ppitch = 424e-6;    %power or ground pad pitch
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-R = rho*H/(pi*(D/2)^2);
+resistivity_bulk = 17.2e-9;
+electron_mfp = 39e-9; % (m) Mean free path of electrons in copper
+specularity_coeff = 0.55;
+reflection_coeff = 0.43;
+width = D;
+height = D;
+wire_length = H;
+[R rho_cu] = xcm.calc_cu_wire_resistance_size_dependent(resistivity_bulk,width,height,wire_length,electron_mfp,specularity_coeff,reflection_coeff); % Use size-dependent resistivity, rather than bulk
+
+R = rho_cu*H/(pi*(D/2)^2);
 Ls = mu*H/(2*pi)*log(1+2.84*H/(pi*(D/2)));
 Lm = 4*0.199*mu*H*log(1+1.0438*H/TSVpitch)-4*0.199*mu*H*log(1+1.0438*H/Ppitch);
 L = Ls+Lm;
