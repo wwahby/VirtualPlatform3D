617c3d15e9733045043efbb00ea31497328ceae2
wwahby <w.wahby@gmail.com>
2014-07-21 10:27:25 -0400

    Updated WLA to prevent overfilling bottom layer

--- model_integration/+xcm/wlatdri.m
+++ model_integration/+xcm/wlatdri.m
@@ -1,14 +1,14 @@
 function [wire repeater] = wlatdri(chip,gate,wire)
 
 bottom_layer_underfilled = 1;
-min_fill_factor = 0.9; % minimum utilization of available bottom layer area
+min_fill_factor = 0.97; % minimum utilization of available bottom layer area
 top_fill_factor = wire.routing_efficiency(1);
 if(length(wire.routing_efficiency) == 1) % if we only have one entry for routing efficiency we need to preserve the nominal value for all the underlying layers
     wire.routing_efficiency = [wire.routing_efficiency(1) wire.routing_efficiency]; % create a separate entry for first layer
 end
 
 % dead man counter to get out of while loop if something goes wrong
-max_wla_attempts = 16;
+max_wla_attempts = 50;
 min_top_fill_factor = 0.01;
 
 lbnd = min_top_fill_factor;
@@ -35,19 +35,30 @@ while( (bottom_layer_underfilled) && (bin_gen <= max_wla_attempts) )
     ntiers_mid = length(wire_mid.pn);
     ntiers_rbnd = length(wire_rbnd.pn);
     
-    bot_ff_lbnd = wire_lbnd.wire_area(1)/wire_lbnd.area_per_layer(1);
-    bot_ff_mid = wire_mid.wire_area(1)/wire_mid.area_per_layer(1);
-    bot_ff_rbnd = wire_rbnd.wire_area(1)/wire_rbnd.area_per_layer(1);
+    area_lbnd = wire_lbnd.wire_area(1) + wire_lbnd.via_area(1);
+    area_mid = wire_mid.wire_area(1) + wire_mid.via_area(1);
+    area_rbnd = wire_rbnd.wire_area(1) + wire_rbnd.via_area(1);
+    
+    bot_ff_lbnd = (area_lbnd )/wire_lbnd.area_per_layer(1);
+    bot_ff_mid = (area_mid )/wire_mid.area_per_layer(1);
+    bot_ff_rbnd = (area_rbnd )/wire_rbnd.area_per_layer(1);
+    
+    
     
     tier_vec = [ntiers_lbnd ntiers_mid ntiers_rbnd];
+    area_avail_vec = [wire_lbnd.area_per_layer(1) wire_mid.area_per_layer(1) wire_rbnd.area_per_layer(1)];
+    area_vec = [area_lbnd area_mid area_rbnd];
     bot_ff_vec = [bot_ff_lbnd bot_ff_mid bot_ff_rbnd];
     [min_tiers best_choice_tiers] = min(tier_vec);
     %[min_ff best_choice_ff] = min(bot_ff_vec);
     
     % First, are there multiple choices with the best number of tiers?
     min_tiers_matches = (tier_vec == min_tiers);
+    area_ok_vec = (area_vec <= area_avail_vec);
     
-    bot_ff_vec(~min_tiers_matches) = -1e9; % kill off any choices with higher tier numbers
+    matches_to_consider = (area_ok_vec & min_tiers_matches);
+
+    bot_ff_vec(~matches_to_consider) = -1e9; % kill off any choices with higher tier numbers
     
     % Now find best ff from among min_tiers possibilities
     [max_ff best_choice] = max(bot_ff_vec);
