9a0392ba4e9cb5620052925557ac26e447564b40
wwahby <w.wahby@gmail.com>
2014-11-12 09:58:15 -0500

    Updates to FREQ/ILD/THICK sweep test

--- model_integration/sweep_sandy_bridge_core_freq_ild_and_tier_thickness_psn_impact.m
+++ model_integration/sweep_sandy_bridge_core_freq_ild_and_tier_thickness_psn_impact.m
@@ -71,113 +71,172 @@ rent_exp_mem = 0.4;
 rent_exp_gpu = 0.55;
 
 %% 
-tiers = [1 2 4 8];
+tiers = 1:8;
 thicknesses = [1e-6 100e-6];
 rel_permittivities = [1 3];
 frequencies = [logspace(8,10,31) 20e9 30e9];
+heat_fluxes = [ h_air h_water];
 
 
 num_stacks = length(tiers);
 num_perms = length(rel_permittivities);
 num_thicks = length(thicknesses);
 num_freqs = length(frequencies);
+num_cooling_configs = length(heat_fluxes);
 
-power = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-power_density = zeros(num_thicks,num_stacks,num_perms,num_freqs);
+power = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+power_density = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
 
-wire_power = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-rep_power = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-temp = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-thickness = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-npads = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-cap_density = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-Ltsv_m2 = zeros(num_thicks,num_stacks,num_perms,num_freqs);
-
-
-ild_cell = cell(num_thicks,num_stacks,num_perms,num_freqs);
-psn_cell = cell(num_thicks,num_stacks,num_perms,num_freqs);
-power_cell = cell(num_thicks,num_stacks,num_perms,num_freqs);
-wire_cell = cell(num_thicks,num_stacks,num_perms,num_freqs);
-chip_cell = cell(num_thicks,num_stacks,num_perms,num_freqs);
-tsv_cell = cell(num_thicks,num_stacks,num_perms,num_freqs);
+wire_power = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+rep_power = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+temp = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+thickness = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+npads = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+cap_density = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+Ltsv_m2 = zeros(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
 
 
-for thind = 1:num_thicks
-    for nind = 1:length(tiers)
-        for pind = 1:num_perms
-            for freq_ind = 1:num_freqs
-                die_thickness = thicknesses(thind);
-                num_layers_per_block = tiers(nind);
-                epsrd = rel_permittivities(pind);
-                fmax_core = frequencies(freq_ind);
+ild_cell = cell(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+psn_cell = cell(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+power_cell = cell(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+wire_cell = cell(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+chip_cell = cell(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
+tsv_cell = cell(num_cooling_configs,num_thicks,num_stacks,num_perms,num_freqs);
 
-                fprintf('====================\n')
-                fprintf('==== Tiers: %d =====\n',tiers(nind));
-                fprintf('==== epsrd: %d =====\n',epsrd);
-                fprintf('==== thickness: %.3g =====\n',die_thickness*1e6);
-                fprintf('==== freq: %.3g =====\n',fmax_core);
-                fprintf('====================\n')
+for cind = 1:num_cooling_configs
+    for thind = 1:num_thicks
+        for nind = 1:length(tiers)
+            for pind = 1:num_perms
+                for freq_ind = 1:num_freqs
+                    die_thickness = thicknesses(thind);
+                    num_layers_per_block = tiers(nind);
+                    epsrd = rel_permittivities(pind);
+                    fmax_core = frequencies(freq_ind);
 
-                %% define parameters
+                    fprintf('====================\n')
+                    fprintf('==== cooling: %d =====\n',cind);
+                    fprintf('==== Tiers: %d =====\n',tiers(nind));
+                    fprintf('==== epsrd: %d =====\n',epsrd);
+                    fprintf('==== thickness: %.3g =====\n',die_thickness*1e6);
+                    fprintf('==== freq: %.3g =====\n',fmax_core);
+                    fprintf('====================\n')
 
-                [core.chip core.transistor core.gate core.tsv core.wire core.psn] = generate_basic_processor_settings(rent_exp_logic,num_layers_per_block,Ng_core,Ach_mm2_core,gate_pitch_core,min_pitch_core,Vdd_core,fmax_core,w_trans);
-                %core.psn.mismatch_tolerance = 0.01;
-                %% Tweak wiring parameters
-                core.wire.repeater_fraction = [0.3]; % 1 is default from gen_basic_proc_settings
-                core.wire.routing_efficiency = [0.6]; % 0.4 is default from gen_basic_proc_settings
-                core.wire.use_graphene = 0;
-                simulation.force_thickness = 1;
-                core.chip.thickness_nominal = die_thickness;
-                core.wire.dielectric_epsr = epsrd;
+                    %% define parameters
+
+                    [core.chip core.transistor core.gate core.tsv core.wire core.psn] = generate_basic_processor_settings(rent_exp_logic,num_layers_per_block,Ng_core,Ach_mm2_core,gate_pitch_core,min_pitch_core,Vdd_core,fmax_core,w_trans);
+                    %core.psn.mismatch_tolerance = 0.01;
+                    %% Tweak wiring parameters
+                    core.wire.repeater_fraction = [0.3]; % 1 is default from gen_basic_proc_settings
+                    core.wire.routing_efficiency = [0.6]; % 0.4 is default from gen_basic_proc_settings
+                    core.wire.use_graphene = 0;
+                    simulation.force_thickness = 1;
+                    core.chip.thickness_nominal = die_thickness;
+                    core.wire.dielectric_epsr = epsrd;
 
 
-                if (die_thickness < 30e-6) % for monolithic-scale chips use thin SiO2 layer rather than underfill
-                    heat.interposer_thickness = 200e-6; % (m) Thickness of the interposer below the 3D stack
-                    heat.bump_thickness = 40e-6;        % (m) Microbump thickness (between interposer and bottom chip of 3D stack)
-                    heat.underfill_thickness = 0.2e-6;    % (m) Thickness of underfill material between each die in the 3D stack
-                    heat.tim_thickness = 5e-6;          % (m) Thickness of thermal interface material between top chip in stack and heat sink
-                    heat.material_IDs = [ 2 9 5];
-                else % for standard die stacking go ahead and use regular underfill
-                    heat.interposer_thickness = 200e-6; % (m) Thickness of the interposer below the 3D stack
-                    heat.bump_thickness = 40e-6;        % (m) Microbump thickness (between interposer and bottom chip of 3D stack)
-                    heat.underfill_thickness = 5e-6;    % (m) Thickness of underfill material between each die in the 3D stack
-                    heat.tim_thickness = 5e-6;          % (m) Thickness of thermal interface material between top chip in stack and heat sink
-                    heat.material_IDs = [ 2 9 3];
+                    if (die_thickness < 30e-6) % for monolithic-scale chips use thin SiO2 layer rather than underfill
+                        heat.interposer_thickness = 200e-6; % (m) Thickness of the interposer below the 3D stack
+                        heat.bump_thickness = 40e-6;        % (m) Microbump thickness (between interposer and bottom chip of 3D stack)
+                        heat.underfill_thickness = 0.2e-6;    % (m) Thickness of underfill material between each die in the 3D stack
+                        heat.tim_thickness = 5e-6;          % (m) Thickness of thermal interface material between top chip in stack and heat sink
+                        heat.material_IDs = [ 2 9 5];
+                    else % for standard die stacking go ahead and use regular underfill
+                        heat.interposer_thickness = 200e-6; % (m) Thickness of the interposer below the 3D stack
+                        heat.bump_thickness = 40e-6;        % (m) Microbump thickness (between interposer and bottom chip of 3D stack)
+                        heat.underfill_thickness = 5e-6;    % (m) Thickness of underfill material between each die in the 3D stack
+                        heat.tim_thickness = 5e-6;          % (m) Thickness of thermal interface material between top chip in stack and heat sink
+                        heat.material_IDs = [ 2 9 3];
+                    end
+                    
+                    heat.up = heat_fluxes(cind);        % above chip
+
+                    %% calculate block parameters
+                    [core.chip core.power core.tsv core.wire core.repeater core.psn] = codesign_block(core.chip,core.tsv,core.gate,core.transistor,core.wire,heat,core.psn,simulation);
+
+                    power(cind,thind,nind,pind,freq_ind) = core.power.total;
+                    power_density(cind,thind,nind,pind,freq_ind) = core.power.density;
+
+                    wire_power(cind,thind,nind,pind,freq_ind) = core.power.wiring;
+                    rep_power(cind,thind,nind,pind,freq_ind) = core.power.repeater;
+                    temp(cind,thind,nind,pind,freq_ind) = core.chip.temperature;
+                    thickness(cind,thind,nind,pind,freq_ind) = core.chip.thickness;
+                    npads(cind,thind,nind,pind,freq_ind) = core.psn.Npads;
+
+
+                    ild_cell{cind,thind,nind,pind,freq_ind} = core.chip.iidf;
+                    psn_cell{cind,thind,nind,pind,freq_ind} = core.psn;
+                    power_cell{cind,thind,nind,pind,freq_ind} = core.power;
+                    wire_cell{cind,thind,nind,pind,freq_ind} = core.wire;
+                    chip_cell{cind,thind,nind,pind,freq_ind} = core.chip;
+                    tsv_cell{cind,thind,nind,pind,freq_ind} = core.tsv;
+
+                    Ltsv_m2(cind,thind,nind,pind,freq_ind) = psn_cell{cind,thind,nind,pind,freq_ind}.Ltsv/psn_cell{cind,thind,nind,pind,freq_ind}.l_unit_cell^2;
+                    cap_density(cind,thind,nind,pind,freq_ind) = psn_cell{cind,thind,nind,pind,freq_ind}.cap_density;
                 end
-
-                %% calculate block parameters
-                [core.chip core.power core.tsv core.wire core.repeater core.psn] = codesign_block(core.chip,core.tsv,core.gate,core.transistor,core.wire,heat,core.psn,simulation);
-
-                power(thind,nind,pind,freq_ind) = core.power.total;
-                power_density(thind,nind,pind,freq_ind) = core.power.density;
-
-                wire_power(thind,nind,pind,freq_ind) = core.power.wiring;
-                rep_power(thind,nind,pind,freq_ind) = core.power.repeater;
-                temp(thind,nind,pind,freq_ind) = core.chip.temperature;
-                thickness(thind,nind,pind,freq_ind) = core.chip.thickness;
-                npads(thind,nind,pind,freq_ind) = core.psn.Npads;
-
-
-                ild_cell{thind,nind,pind,freq_ind} = core.chip.iidf;
-                psn_cell{thind,nind,pind,freq_ind} = core.psn;
-                power_cell{thind,nind,pind,freq_ind} = core.power;
-                wire_cell{thind,nind,pind,freq_ind} = core.wire;
-                chip_cell{thind,nind,pind,freq_ind} = core.chip;
-                tsv_cell{thind,nind,pind,freq_ind} = core.tsv;
-
-                Ltsv_m2(thind,nind,pind,freq_ind) = psn_cell{thind,nind,pind,freq_ind}.Ltsv/psn_cell{thind,nind,pind,freq_ind}.l_unit_cell^2;
-                cap_density(thind,nind,pind,freq_ind) = psn_cell{thind,nind,pind,freq_ind}.cap_density;
             end
-        end
 
+        end
     end
 end
+
+%% Find the maximum frequency that the device can run to stay below 90C
+Tmax = 90;
+fmax_temp_limited = zeros(num_cooling_configs,num_thicks,num_perms,length(tiers));
+
+for cind = 1:num_cooling_configs
+    for thind = 1:num_thicks
+        for nind = 1:length(tiers)
+            for pind = 1:num_perms
+                Tind = find( (temp(cind,thind,nind,pind,:) <= Tmax), 1, 'last');
+                fmax_temp_limited(cind,thind,pind,nind) = frequencies(Tind);
+            end
+        end
+    end
+end
+
+
+%% Plot frequency chart
+figure(1)
+clf
+%set(gcf,'DefaultAxesColorOrder',[0 0 1; 1 0 0 ])
+%set(gcf,'DefaultAxesLineStyleOrder','-|--|-.|:')
+%hold all
+hold on
+
+plot_colors = [1 0 0; 0 0 1; 1 0 0; 0 0 1; 1 0 1; 0 1 0; 1 0 1; 0 1 0];
+plot_styles = {'-','-','--','--','-','-','--','--'};
+
+plot_ind = 1;
+for cind = 1:num_cooling_configs
+    for thind = num_thicks:-1:1
+        for pind = num_perms:-1:1
+            
+            fmax_tl_plot = zeros(1,length(tiers));
+            for nind = 1:length(tiers)
+                fmax_tl_plot(nind) = fmax_temp_limited(cind,thind,pind,nind);
+            end
+            
+            plot(tiers,fmax_tl_plot,'color',plot_colors(plot_ind,:),'linestyle',plot_styles{plot_ind})
+            plot_ind = plot_ind + 1;
+        end
+    end
+end
+set(gca,'yscale','log')
+%ylim([5e8 0.5e10])
+xlabel('Number of tiers')
+ylabel('Frequency (Hz)')
+fixfigs(1,3,14,12)
+
+
+
+
+
 
 % %%
 % % for thind = 1:num_thicks
 % %     for pind = 1:num_perms
 % %         for nind = 1:length(num_layers)
-% %             power_density(thind,nind,pind,freq_ind) = power_cell{thind,nind,pind,freq_ind}.density;
+% %             power_density(cind,thind,nind,pind,freq_ind) = power_cell{cind,thind,nind,pind,freq_ind}.density;
 % %         end
 % %     end
 % % end
@@ -204,7 +263,7 @@ end
 %         npads_plot = zeros(1,length(tiers));
 % 
 %         for nind = 1:length(tiers)
-%             npads_plot(nind) = npads(thind,nind,pind,freq_ind);
+%             npads_plot(nind) = npads(cind,thind,nind,pind,freq_ind);
 %         end
 %         
 %         plot(tiers,npads_plot)
@@ -235,7 +294,7 @@ end
 %         temp_plot = zeros(1,length(tiers));
 % 
 %         for nind = 1:length(tiers)
-%             temp_plot(nind) = temp(thind,nind,pind,freq_ind);
+%             temp_plot(nind) = temp(cind,thind,nind,pind,freq_ind);
 %         end
 %         
 %         plot(tiers,temp_plot)
@@ -265,7 +324,7 @@ end
 %         pow_plot = zeros(1,length(tiers));
 % 
 %         for nind = 1:length(tiers)
-%             pow_plot(nind) = power(thind,nind,pind,freq_ind);
+%             pow_plot(nind) = power(cind,thind,nind,pind,freq_ind);
 %         end
 %         
 %         plot(tiers,pow_plot)
@@ -293,7 +352,7 @@ end
 %         pdens_plot = zeros(1,length(tiers));
 % 
 %         for nind = 1:length(tiers)
-%             pdens_plot(nind) = power_density(thind,nind,pind,freq_ind)/1e4;
+%             pdens_plot(nind) = power_density(cind,thind,nind,pind,freq_ind)/1e4;
 %         end
 %         
 %         plot(tiers,pdens_plot)
