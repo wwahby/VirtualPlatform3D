0aa8513ccc14a4d2a05a8af5d23c06d785ede43b
wwahby <w.wahby@gmail.com>
2015-04-09 11:18:30 -0400

    Updated/cleaned penryn fullchip sim test case to work properly with latest simulation flow
    Mostly just moving things out of the test case header and into the
    setting generator. Chip-specific settings are now just set after the
    generator call.

--- model_integration/test_codesign_system_best_penryn_fit_fullchip.m
+++ model_integration/test_codesign_system_best_penryn_fit_fullchip.m
@@ -38,122 +38,20 @@ w_trans = 45e-9;
 eps0 = 8.854e-12; % (F/m) vacuum permittivity
 %gate_pitch = sqrt( Ach_mm2/1e6/Ng);
 
-%% ==================================================
-%  ================ BEGIN SIMULATION ================
-%  ==================================================
-%% Chip parameters
-chip.num_gates = Ng;            % (-) Number gates in the system
-chip.num_layers = S;            % (-) Number of layers in the 3D stack
-chip.area_total = Ach_mm2*1e-6; % (m2) Total chip area
-chip.min_pitch = min_pitch;     % (m) Minimum printable pitch (generally contacted gate pitch)
-chip.gate_pitch = gate_pitch;   % (m) Actual average gate pitch
+rent_exp_logic = 0.6;
+num_layers_per_block = 1;
+Ng_core = Ng;
+Ach_mm2_core = Ach_mm2;
+gate_pitch_core = gate_pitch;
+min_pitch_core = min_pitch;
+Vdd_core = 1.3625;
+fmax_core = fmax;
 
-chip.fanout = 4;        % average fanout
-chip.alpha = chip.fanout/(chip.fanout+1); % input terminal fraction
-chip.rent_p = 0.6;      % rent exponent
-chip.rent_k = 3/chip.alpha;  % rent constant
-chip.chi = 2/3;         % (-) Conversion factor for point-to-point interconnect length and total net length
-
-chip.clock_period = 1/fmax; % (s) Clock period
-chip.logic_activity_factor = 0.1; % (-) Fraction of gates switching at every clock cycle
-chip.Vdd = 1.3625;        % (V) Supply voltage
-chip.temperature = 25;  % (deg C) Temperature
-chip.thickness_nominal = 50e-6; % (m) Nominal substrate thickness
-
-%% Transistor and gate parameters
-transistor.gate_length = w_trans;
-transistor.oxide_rel_permittivity = 25; % HfO2
-transistor.oxide_thickness = 1e-9;
-transistor.leakage_current_per_micron = 10e-9; %(A/um)
-transistor.capacitance = transistor.oxide_rel_permittivity*eps0*transistor.gate_length^2/transistor.oxide_thickness;
-transistor.subthreshold_swing = .060; % (V/decade at 300K)
-transistor.Vt = 0.25; % (V) - Threhsold voltage
-
-gate.output_resistance = 8e3;   % (Ohm) Output resistance of a minimum-sized 2in NAND gate
-gate.num_transistors = 4;       % (-) number of transistors per average logic gate
-gate.capacitance = gate.num_transistors*transistor.capacitance;
-
-%% TSV parameters
-tsv.aspect_ratio = 20;          % (-) TSV height / TSV width
-tsv.max_area_fraction = 0.10;   % (-) % Maximum fraction of total chip area that TSVs are allowed to consume
-
-
-%% Wiring parameters
-wire.aspect_ratio = 1.8;        % (-) h/w of wires in metal layers
-wire.width_fraction = 0.5;     % (-) width/pitch of wires in metal layers
-wire.resistivity = 17.2e-9;     % (Ohm*m) Copper wires
-wire.permeability_rel = 1;      % (-) Relative permeability of wiring material
-wire.dielectric_epsr = 3.0;     % (-) Relative dielectric constant for wiring ILD -- Low-K dielectric
-wire.layers_per_tier = 1;       % (-) Number of metal layers sharing same pitch in each tier
-wire.routing_efficiency = [ 0.45 ];  % (-) Fraction of available area that the wire routing tool can actually use
-wire.repeater_fraction = [ 0.4 ]; % (-) fraction of optimal repeaters to insert
-wire.Beta = [0.9];              % (-v) Fraction of total clock period that a single point-to-point interconnect can consume
-wire.Beta_short = 0.25;         % (-) Beta for shortest wiring layers (used for the top down WLARI)
-wire.Rc = 0;                    % (-v) Contact resistance between tiers (can be a vector)
-wire.use_graphene = 0;
-
-%% Power supply noise model parameters
-
-psn.noise_fraction = 0.15;          % (-) Maximum power supply noise as a fraction of Vdd
-psn.noise_target = 0.1875;          % (V) Acceptable power supply noise
-psn.decap_area_fraction = 0.1;      % (Ratio) - Fraction of chip area dedicated to decoupling capacitors
-
-% Power connection parameters
-psn.Npads_1d = 100;                  % (-) Number of pads from one side of chip to the other
-psn.Npads = psn.Npads_1d^2;         % (-) total number of power pads (does not include ground pads)
-psn.Ngrid = 21*21;                  % (-) grid fineness
-psn.pad_size = 1;                   % (-) Pad size is in terms of segment number
-psn.segment_thickness = 1e-6;       % (m) Thickness of a power grid segment
-psn.segment_width = 2e-6;           % (m) width of a power grid segment
-
-% Package parameters
-psn.package_resistance = 0.006;     % (Ohm) Resistance per pad on the package
-psn.package_inductance = 0.5e-9;    % (H) Inductance per package pad
-
-% Power TSV determination
-psn.mismatch_tolerance = 0.05;      % (-) Allowable normalized deviation from noise target
-
-mu_m = 1.257e-6;      %copper permeability
-
-% decap = 0.1; % (Ratio) - Fraction of chip area dedicated to decoupling capacitors
-% Npad1d = 32;
-% Npad = Npad1d^2;         %total number of power or ground pads
-% Ngrid = 21*21;        %grid fineness
-% padsize = 1;          % Pad size is in terms of segment number
-% Tseg = 1e-6;          % Thickness of grid segment
-% Wseg = 2e-6;          % width of grid segment
-
-%% Thermal parameters
-%the heat transfer coefficient
-% r = 1/(hA); A is the size of top surface area
-% the cooling capability of the top heatsink; 20000, 1cm*1cm, means:
-% 0.5 W/K
-% h = q/dT - q = heat flux (W/m^2)
-heat.up = 20000;
-
-% Bottom surface heat transfer coefficient
-% This parameter controls the area directly BELOW the bottom chip
-% If the interposer is larger than the bottom chip, heat.d controls the
-% rest of the area
-% Microfluidic heat sinks are assumed to be as large as the chip in the interposer
-heat.down = 5;  
-
-% Heat transfer coefficient for the interposer area NOT directly underneath
-% the chip(s)
-heat.d = 5;
-
-% Side surface heat coefficient, usually near adiabatic
-heat.side = 5;
-
-heat.Ta = 298; % ambient temperature
-
-% Alternative settings
-% q_cm2 = 50; % (W/cm2) Top heat sink max heat flux
-% q = q_cm2*1e4; % (W/m2) Top heat sink max heat flux
-% dT = 70; % (deg C) Temp difference between chip surface and coolant (air)
-% heat.up = q/dT;
-% heat.down = 2*heat.up;
-% heat.d = heat.down;
+[chip, transistor, gate, tsv, wire, psn, heat] = generate_basic_processor_settings(rent_exp_logic,num_layers_per_block,Ng_core,Ach_mm2_core,gate_pitch_core,min_pitch_core,Vdd_core,fmax_core,w_trans);
+%wire.routing_efficiency = [ 0.45 ];  % (-) Fraction of available area that the wire routing tool can actually use
+%wire.repeater_fraction = [ 0.4 ]; % (-) fraction of optimal repeaters to insert
+wire.routing_efficiency = 0.5;
+wire.repeater_fraction = 0.4;
 
 %% Simulation parameters
 simulation.use_joyner = 0;
@@ -163,6 +61,7 @@ simulation.skip_psn_loops = 1; % Skip PSN TSV homing for faster debug
 simulation.draw_thermal_map = 1; % Plot thermal profile of each chip
 simulation.print_thermal_data = 0; % Output max temp in each layer to console
 simulation.separate_wiring_tiers = 0;
+simulation.force_thickness = 1;
 
 
 
