e8c2d304e20232f2dc497cb0653401c6aa884820
wwahby <w.wahby@gmail.com>
2015-04-29 17:20:32 -0400

    [BROKEN] Added brute force function for terms 2 and 3, but it doesn't work yet
    Just returns zeros for everything. Not sure why

---
+++ model_integration/+xcm/calc_mid_terms_brute_force.m
@@ -0,0 +1,70 @@
+function [term2, term3] = calc_mid_terms_brute_force(Lx, Nuc_1d, w_tsv)
+
+
+w_uc = Lx/Nuc_1d;
+forbidden_min = round(w_uc/2) - round(w_tsv/2);
+forbidden_max = forbidden_min + w_tsv;
+
+T = Nuc_1d;
+t = forbidden_min;
+
+r = @(x,start,width) (x >= start) && (x <= start + width);
+
+
+fx = @(x,Lx) r(x,0,Lx-1);
+q = @(x_uc,y_uc,t,w) r(x_uc,t,w) * r(y_uc,t,w);
+
+
+lmax = 2*Lx;
+term2 = zeros(1,lmax+1);
+term3 = zeros(1,lmax+1);
+Ly = Lx;
+Nx = Lx;
+Ny = Ly;
+
+w = w_tsv;
+for l_ind = 1:lmax+1
+    l = l_ind - 1
+    lxmax = min(l,Lx);
+    for lx_ind = 1:lxmax+1
+        lx = lx_ind - 1;
+        for x1_ind = 1:Nx
+            x1 = x1_ind - 1;
+            x1_uc = mod(x1,T);
+            x2 = x1 - lx;
+            x2_uc = mod(x2,T);
+            x2_nogo = x2 < 0;
+            if (~x2_nogo)
+                for y1_ind = 1:Ny
+                    y1 = y1_ind - 1;
+                    y1_uc = mod(y1,T);
+                    ly = l - lx;
+                    y2 = y1 - ly;
+                    y2_uc = mod(y2,T);
+                    y2_nogo = y2 < 0;
+                    
+                    p2_nogo = x2_nogo && y2_nogo;
+
+                    if(~p2_nogo)
+                        %a1 = fx(x1,Lx)*fx(y1,Ly); % guaranteed to be fine
+                        %a2 = fx(x2,Lx)*fx(y2,Ly); % guaranteed to be fine
+
+                        %b1 = q(x1_uc,y1_uc,t,w_tsv);
+                        b1x = (x1_uc < t) && (x1_uc > t + w);
+                        b1y = (y1_uc < t) && (y1_uc > t + w);
+                        b1 = b1x*b1y;
+                        
+                        b2x = (x2_uc < t) && (x2_uc > t + w);
+                        b2y = (y2_uc < t) && (y2_uc > t + w);
+                        b2 = b2x*b2y;
+                        %b2 = q(x2_uc,y2_uc,t,w_tsv);
+
+                        term2(l_ind) = term2(l_ind) + b2;
+                        term3(l_ind) = term3(l_ind) + b1;
+                    end
+                end
+            end
+        end
+    end
+end
+    
