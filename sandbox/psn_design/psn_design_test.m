7fe3d308d11d60e55fad0bb11424b42aa02b5964
wwahby <w.wahby@gmail.com>
2015-06-03 19:35:12 -0700

    Added standalone psn test in sandbox

---
+++ sandbox/psn_design/psn_design_test.m
@@ -0,0 +1,38 @@
+% Add VP3D to path for simulation setup
+addpath C:\Users\wsw\Documents\GitHub\VirtualPlatform3D\model_integration
+
+area = (100e-3)^2; %m2
+power = 100; % W
+
+wire.rho_vec = 16.8e-9; % Cu
+wire.permeability_rel = 1; % nonmagnetic
+chip.area_per_layer_m2 = area;
+chip.num_layers = 64;
+chip.temperature = 300; % K
+chip.Vdd = 1;
+
+psn.decap_area_fraction = 0.1;
+tsv.height_m = 50e-6;
+
+psn.noise_target = 0.1875;          % (V) Acceptable power supply noise
+psn.decap_area_fraction = 0.1;      % (Ratio) - Fraction of chip area dedicated to decoupling capacitors
+psn.power_tsv_width = 10e-6;        % (m) Width of TSVs used for power delivery. This may be larger than signal TSVs. Setting this to -1 will cause PSN module to use the signal TSV width.
+
+% Power connection parameters
+psn.Npads_1d = 100;                  % (-) Number of pads from one side of chip to the other
+psn.Npads = psn.Npads_1d^2;         % (-) total number of power pads (does not include ground pads)
+psn.Ngrid = 21*21;                  % (-) grid fineness
+psn.pad_size = 1;                   % (-) Pad size is in terms of segment number
+psn.segment_thickness = 1e-6;       % (m) Thickness of a power grid segment
+psn.segment_width = 2e-6;           % (m) width of a power grid segment
+
+% Package parameters
+psn.package_resistance = 0.006;     % (Ohm) Resistance per pad on the package
+psn.package_inductance = 0.5e-9;    % (H) Inductance per package pad
+
+% Power TSV determination
+psn.mismatch_tolerance = 0.01;      % (-) Allowable normalized deviation from noise target
+
+power.density = power/area;
+
+psn = determine_power_tsv_requirements(tsv,psn,power,wire,chip);
