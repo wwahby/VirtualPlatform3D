2587695d9419b73db0c60a6656e618565dfd34ca
wwahby <w.wahby@gmail.com>
2015-11-05 16:51:27 -0500

    Added test script for quick leakage estimation

---
+++ model_integration/test_leakage.m
@@ -0,0 +1,22 @@
+
+w_trans = 32e-9;
+Ioff_per_um = 100e-9;
+
+width_um = 1.5*w_trans*1e6;
+w_trans_um = w_trans * 1e6;
+Ilk_To = Ioff_per_um*width_um;
+To = 300; % (K)
+Vdd = 1.25;
+Vds = Vdd;
+
+kb = 1.381e-23; % (J/K)
+T = 390; % (K)
+q = 1.602e-19; % (C)
+Eg = 1.12; % (eV)
+
+phi_th = kb*T/q;
+Ilk_T = Ilk_To * (T/To)*exp(-q*Eg/kb*(1/T-1/To))*(exp(-Vds/phi_th)-1);
+Ilk_T = abs(Ilk_T);
+
+Plk_per_transistor = Ilk_T*Vdd
+Plk_per_transistor_per_um = Ilk_T/width_um
