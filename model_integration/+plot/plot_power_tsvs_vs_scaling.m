7b84bc808e75d757fe6c5c59b29b919b8ffedc0e
wwahby <w.wahby@gmail.com>
2016-02-17 18:45:50 -0500

    Added frequency plot to power tsv scaling (in case of thermal binsearch)

--- model_integration/+plot/plot_power_tsvs_vs_scaling.m
+++ model_integration/+plot/plot_power_tsvs_vs_scaling.m
@@ -140,6 +140,7 @@ num_metal_levels_mat = zeros(num_wire_resistivities, num_stacks, num_scaling_fac
 npads_mat = zeros(num_wire_resistivities, num_stacks, num_scaling_factors);
 power_mat = zeros(num_wire_resistivities, num_stacks, num_scaling_factors);
 temp_mat = zeros(num_wire_resistivities, num_stacks, num_scaling_factors);
+freq_mat = zeros(num_wire_resistivities, num_stacks, num_scaling_factors);
 for nind = 1:num_stacks
     for wire_res_ind = 1:num_wire_resistivities
         for scaling_ind = 1:num_scaling_factors
@@ -148,11 +149,14 @@ for nind = 1:num_stacks
                 temp_mat(wire_res_ind, nind, scaling_ind) = temperature(cind,dind,thind,nind,pind,freq_ind,wire_res_ind,wire_flag_ind,scaling_ind,bar_thick_ind,bar_res_ind,forced_power_ind,k_ind);
                 npads_mat(wire_res_ind, nind, scaling_ind) = npads(cind,dind,thind,nind,pind,freq_ind,wire_res_ind,wire_flag_ind,scaling_ind,bar_thick_ind,bar_res_ind,forced_power_ind,k_ind);
                 num_metal_levels_mat(wire_res_ind, nind, scaling_ind) = num_metal_levels(cind,dind,thind,nind,pind,freq_ind,wire_res_ind,wire_flag_ind,scaling_ind,bar_thick_ind,bar_res_ind,forced_power_ind,k_ind);
+                freq_mat(wire_res_ind, nind, scaling_ind) = freq(cind,dind,thind,nind,pind,freq_ind,wire_res_ind,wire_flag_ind,scaling_ind,bar_thick_ind,bar_res_ind,forced_power_ind,k_ind);
+
             else
                 power_mat(wire_res_ind, nind, scaling_ind) = 0;
                 temp_mat(wire_res_ind, nind, scaling_ind) = 0;
                 npads_mat(wire_res_ind, nind, scaling_ind) = 0;
                 num_metal_levels_mat(wire_res_ind, nind, scaling_ind) = 0;
+                freq_mat(wire_res_ind, nind, scaling_ind) = 0;
             end
                 
         end
@@ -287,6 +291,26 @@ set(gca, 'xtick', 1:num_scaling_factors)
 set(gca, 'xticklabel', node_labels)
 set(gca,'yscale','log')
 fixfigs(12,3,14,12)
+
+figure(13)
+clf
+hold on
+for wire_res_ind = 1:num_wire_resistivities
+    for nind = 1:num_stacks
+        plot_vec = zeros(1,num_scaling_factors);
+        plot_vec(1,:) = freq_mat(wire_res_ind, nind,:);
+        plot_vec = plot_vec( plot_vec > 0);
+
+        plot(1:length(plot_vec), plot_vec/1e9, 'color', colors{wire_res_ind}, 'linestyle', linestyles{nind});
+    end
+end
+xlabel('Process Node')
+ylabel('Maximum Clock Frequency (GHz)')
+set(gca, 'xtick', 1:num_scaling_factors)
+set(gca, 'xticklabel', node_labels)
+%set(gca,'yscale','log')
+fixfigs(13,3,14,12)
+
 
 
 
