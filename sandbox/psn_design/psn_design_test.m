b693112ba4bc8101147a7a10b05dc8bdc404c921
wwahby <w.wahby@gmail.com>
2016-07-12 12:10:08 -0400

    Updated sandbox psn design script

--- sandbox/psn_design/psn_design_test.m
+++ sandbox/psn_design/psn_design_test.m
@@ -1,8 +1,8 @@
 % Add VP3D to path for simulation setup
-addpath C:\Users\wsw\Documents\GitHub\VirtualPlatform3D\model_integration
+addpath('../../model_integration/')
 
 area = (sqrt(2)*1e-3)^2; %m2
-power = 2e-3; % W
+core_power = 2e-3; % W
 
 wire.rho_vec = 16.8e-9; % Cu
 wire.permeability_rel = 1; % nonmagnetic
@@ -13,8 +13,11 @@ chip.Vdd = 1;
 
 psn.decap_area_fraction = 0.1;
 tsv.height_m = 50e-6;
+tsv.barrier_thickness = 0e-9;
+tsv.barrier_resistivity = 17.2e-9;
 
 psn.noise_target = 0.1875;          % (V) Acceptable power supply noise
+psn.noise_fraction = 0.10;
 psn.decap_area_fraction = 0.1;      % (Ratio) - Fraction of chip area dedicated to decoupling capacitors
 psn.power_tsv_width = 10e-6;        % (m) Width of TSVs used for power delivery. This may be larger than signal TSVs. Setting this to -1 will cause PSN module to use the signal TSV width.
 
@@ -33,6 +36,6 @@ psn.package_inductance = 0.5e-9;    % (H) Inductance per package pad
 % Power TSV determination
 psn.mismatch_tolerance = 0.01;      % (-) Allowable normalized deviation from noise target
 
-power.density = power/area;
+power.density = core_power/area;
 
 psn = determine_power_tsv_requirements(tsv,psn,power,wire,chip);
